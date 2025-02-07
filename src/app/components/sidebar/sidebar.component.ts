1587834057b5817df7ca309026d5e964262936f5
Budesh Patil <bpatil775@gmail.com>
2025-02-07 10:03:02 +0530

    code commit for admin changes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,27 +9,28 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'ni ni-chart-pie-35 text-primary', class: '' },
-    { path: '/about/view', title: 'About',  icon:'ni ni-paper-diploma text-black', class: '' },
+    // { path: '/about/view', title: 'About',  icon:'ni ni-paper-diploma text-black', class: '' },
     // { path: '/achievement/view', title: 'Achievement',  icon:'ni ni-archive-2 text-black', class: '' },
     // { path: '/award/view', title: 'Award',  icon:'ni ni-archive-2 text-red', class: '' },
-    { path: '/banner-images/view', title: 'Banners',  icon: 'ni ni-image text-orange', class: '' },
 		{ path: '/tvshow/view', title: 'Clients',  icon:'ni-planet text-blue', class: '' },
+    { path: '/contact/list', title: 'Contacts',  icon:'ni ni-chat-round text-green', class: '' },
 		{ path: '/homegallery/view', title: 'Home Portfolio',  icon:'ni ni-album-2 text-pink', class: '' },
+    { path: '/banner-images/view', title: 'Home Page Banners',  icon: 'ni ni-image text-orange', class: '' },
     // { path: '/catalog/view', title: 'Catalog',  icon:'ni-align-left-2 text-red', class: '' },
     // { path: '/client/view', title: 'Client',  icon:'ni ni-archive-2 text-gray', class: '' },
-    { path: '/contact/list', title: 'Contacts',  icon:'ni ni-chat-round text-green', class: '' },
+    
     // { path: '/course/view', title: 'Course',  icon:'ni ni-bulb-61 text-yellow', class: '' },
     // { path: '/download/view', title: 'Downloads',  icon:'ni-align-left-2 text-red', class: '' },
     // { path: '/gallery/view', title: 'Gallery',  icon:'ni ni-bulb-61 text-pink', class: '' },
 
 		// { path: '/socialactivity/view', title: 'Impact',  icon: 'ni ni-settings text-red', class: '' },
 		// { path: '/blog/view', title: 'Media',  icon:'ni ni-book-bookmark text-blue', class: '' },
-    { path: '/project/view', title: 'Projects',  icon:'ni ni-paper-diploma text-black', class: '' },
+    { path: '/project/view', title: 'Portfolio',  icon:'ni ni-paper-diploma text-black', class: '' },
     // { path: '/podcast/view', title: 'Podcast',  icon: 'ni-bullet-list-67 text-red', class: '' },
     { path: '/page/view', title: 'SEO Pages',  icon: 'ni-align-left-2 text-blue', class: '' },
 
     // { path: '/student/view', title: 'Students',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/subscriber/view', title: 'Subscribers',  icon:'ni ni-active-40 text-gray', class: '' },
+    // { path: '/subscriber/view', title: 'Subscribers',  icon:'ni ni-active-40 text-gray', class: '' },
     { path: '/testimonial/view', title: 'Testimonials',  icon:'ni ni-caps-small text-green', class: '' },
 
     // { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
